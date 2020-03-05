ec106b6491f56a5c6f13286dc4fe87bf1b4cbea3
davidluna1995 <david.luna.cuevas@gmail.com>
2020-03-05 18:01:48 -0300

    APIController

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,8 @@
 
 use Illuminate\Http\Request;
 
-Route::post('auth/login', 'ApiController@login');
-Route::post('register', 'ApiController@register');
+Route::post('auth/login', 'APIController@login');
+Route::post('register', 'APIController@register');
 //Rutas para el reinicio de password
 Route::post('sendEmail','ResetPasswordController@sendEmail');
 Route::post('resetPassword','ResetPasswordController@process');
@@ -14,14 +14,14 @@ Route::post('resetPassword','ResetPasswordController@process');
 Route::group(['middleware' => 'auth.jwt'], function () {
 
     //login y user
-    Route::get('auth/user', 'ApiController@user');
-    Route::post('auth/logout', 'ApiController@logout');
-    Route::get('allUser', 'ApiController@allUser');
-    Route::post('modificar_perfil', 'ApiController@modificar_perfil');
-    Route::post('cambiar_password', 'ApiController@cambiar_password');
-    Route::post('crear_usuario', 'ApiController@crear_usuario');
-    Route::get('traer_usuarios', 'ApiController@traer_usuarios');
-    Route::post('delete_usuario', 'ApiController@delete_usuario');
+    Route::get('auth/user', 'APIController@user');
+    Route::post('auth/logout', 'APIController@logout');
+    Route::get('allUser', 'APIController@allUser');
+    Route::post('modificar_perfil', 'APIController@modificar_perfil');
+    Route::post('cambiar_password', 'APIController@cambiar_password');
+    Route::post('crear_usuario', 'APIController@crear_usuario');
+    Route::get('traer_usuarios', 'APIController@traer_usuarios');
+    Route::post('delete_usuario', 'APIController@delete_usuario');
     
     // rutas categoria
     Route::post('registro_categoria', 'CategoriaController@registro_categoria');
